f9588daeaacacce61a50bf5c00294f9209b13206
RedCatHot <84961728+RedCatHot@users.noreply.github.com>
2022-12-26 10:20:11 +0200

    Add files via upload

---
+++ Database/Database/Program.cs
@@ -0,0 +1,11 @@
+﻿using EssentialDatabase.Core;
+using EssentialDatabase.CLI;
+using EssentialDatabase.Management;
+
+const string storeDbPath = @"C:\Users\LiubomyrMaievskyi\source\repos\Education";
+
+PersistenceService persistenceService = new(storeDbPath);
+DatabaseManager databaseManager = new(persistenceService);
+
+CLIManager cliManager = new CLIManager(databaseManager);
+cliManager.StartConsoleReading();
